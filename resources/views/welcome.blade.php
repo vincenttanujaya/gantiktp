8d668defb4cce2313a9734359b7e53da5d2dfbf1
Daniel Kurniawan <dekaslr8@gmail.com>
2019-04-24 22:30:56 +0700

    remove logout modal + add dynamic file input

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -135,7 +135,7 @@
                       </div>
                     </div>
                     <hr>
-                    <div class="row form-group" id="hilang'>
+                    <div class="row form-group" id="hilang">
                       <div class="col-3">
                         <h1 class="h4 mb-2 text-gray-800">Upload Surat Kehilangan</h1>
                       </div>
@@ -183,23 +183,7 @@
   </a>
 
   <!-- Logout Modal-->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
-        </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-        <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
-        </div>
-      </div>
-    </div>
-  </div>
+
 
   <!-- Bootstrap core JavaScript-->
   <script src="vendor/jquery/jquery.min.js"></script>
@@ -220,8 +204,19 @@
   <script src="js/demo/chart-bar-demo.js"></script>
 
   <script type="text/javascript">
+    $(document).ready(function() {
+      $('#rusak').show();
+      $('#hilang').hide();
+    });
     function inputFunction(element) {
-      if (elemnt.value) {}
+      if (element.value == 'rusak') {
+        $('#rusak').show();
+        $('#hilang').hide();
+      }
+      else {
+        $('#rusak').hide();
+        $('#hilang').show();
+      }
     }
   </script>
 
